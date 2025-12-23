0cc47b06cf5ca71b40e37ac89fd24b4605057b60
Slavi Cvetanov <slavi.cvetanov@yarillo.com>
2025-12-23 12:15:04 +0200

    Fix rotate on swipe

--- src/style.ts
+++ src/style.ts
@@ -133,6 +133,8 @@ export const viewerCircular: CSS.Properties = {
   fontSize: "12",
   fontWeight: 300,
   margin: "auto",
+  // allow custom touch gestures like rotate without the browser cancelling the pointer stream
+  touchAction: "none",
 };
 
 export const circularLabel: CSS.Properties = {
