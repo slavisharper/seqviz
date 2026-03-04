48b047032f4c8c138ba2ee85c551726829d075a8
Slavi Cvetanov <slavi.cvetanov@yarillo.com>
2026-03-04 15:41:08 +0200

    Fix primer label font color with darker bg colors

--- src/viewers/Linear/Primers.tsx
+++ src/viewers/Linear/Primers.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { InputRefFunc } from "../../SelectionHandler";
-import { COLOR_BORDER_MAP, darkerColor } from "../../core/colors";
+import { COLOR_BORDER_MAP, contrastText, darkerColor } from "../../core/colors";
 import { Primer } from "../../core/elements";
 import { annotation, annotationLabel } from "../../style";
 import { FindXAndWidthElementType } from "./SeqBlock";
@@ -250,7 +250,7 @@ const SingleNamedElement = (props: {
         dominantBaseline="middle"
         fontSize={fontSize}
         id={element.id}
-        style={annotationLabel}
+        style={{ ...annotationLabel, fill: color ? contrastText(color) : annotationLabel.fill }}
         textAnchor="middle"
         x={width / 2}
         y={height / 2 + 1}
